c7f2d69983bd4d2b7e92a3cd76f2ac2ddc9e5c78
Fanny Monet <monet.f@sfeir.com>
2019-09-17 10:38:12 +0200

    add of the action types and visibility filters

--- src/action.js
+++ src/action.js
@@ -1,3 +1,22 @@
+/*
+ * action types
+ */
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+/*
+ * other constants
+ */
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}  
+
+/*
+ * action creators
+ */
 const addTodo = text =>  ({type: 'ADD_TODO', text});
 const toggleTodo = id => ({type: 'TOGGLE_TODO', id});
 const setVisibilityFilter = filter => ({type: 'SET_VISIBILITY_FILTER', filter});
