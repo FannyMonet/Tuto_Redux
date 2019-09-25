c31ddfc7f87e4e1a03e4c0bb8a969b26389d362a
Fanny Monet <monet.f@sfeir.com>
2019-09-25 11:46:17 +0200

    add the visibility filter reducer

---
+++ src/reducers/visibilityFilter.test.js
@@ -0,0 +1,13 @@
+import visibilityFilterReducer from './visibilityFilter';
+import { VisibilityFilters, SET_VISIBILITY_FILTER } from '../actions/index';
+
+describe('visibility filter reducer', () => {
+    it('should return the initial state', () => {
+        expect(visibilityFilterReducer(undefined, {})).toEqual(VisibilityFilters.SHOW_ALL);
+    });
+
+    it('should set the visibility filter', () => {
+        expect(visibilityFilterReducer(undefined, { type: SET_VISIBILITY_FILTER, filter: VisibilityFilters.SHOW_COMPLETED }))
+        .toEqual(VisibilityFilters.SHOW_COMPLETED);
+    })
+});
